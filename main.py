bff29ced7233909a7a615777f3d652bdb7aeeeb5
eto1972 <eto1972@gmail.com>
2023-12-24 23:01:11 +0900

    マスターの初版

---
+++ main.py
@@ -0,0 +1,10 @@
+from machine import Pin
+import time
+
+led = Pin('LED', Pin.OUT)  # Pico W では、内蔵LEDの指定はGPIO番号ではなく"LED"を指定
+
+while True:
+    led.on()
+    time.sleep(0.5)
+    led.off()
+    time.sleep(0.5)
